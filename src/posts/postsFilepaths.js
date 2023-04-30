cbbe75d992f3608f50ddedbb3d37ad63b312134b
ashtonsprunger <sprungerashton@gmail.com>
2023-04-30 19:43:06 -0400

    firebase

--- src/posts/postsFilepaths.js
+++ src/posts/postsFilepaths.js
@@ -1,11 +1,7 @@
-const BASE_URL = process.env.NODE_ENV === "development" ? "" : "/web-dev-blog";
-
 const postsFilepaths = [
-  BASE_URL +
-    process.env.PUBLIC_URL +
+  process.env.PUBLIC_URL +
     "/api/posts/keeping-manual-testing-concepts-in-mind.md",
-  BASE_URL +
-    process.env.PUBLIC_URL +
+  process.env.PUBLIC_URL +
     "/api/posts/the-value-of-seo-to-front-end-developers.md",
 ];
 
